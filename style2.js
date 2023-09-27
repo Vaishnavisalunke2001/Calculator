cfa2462b05cb60cd5581608ac7e0f9a86fe875aa
Vaishnavisalunke2001 <125184427+Vaishnavisalunke2001@users.noreply.github.com>
2023-09-27 21:07:17 +0530

    style2.js

--- style2.js
+++ style2.js
@@ -5,8 +5,11 @@ let button_input = document.querySelectorAll(".input-button");
 // access input,equal,clear and erase
 let input = document.getElementById("input");
 let equal = document.getElementById("equal");
-let clear = document.getElementById("clear");
 let erase = document.getElementById("erase");
+
+function clearScreen(){
+    document.getElementById("input").value="";
+}
 
 window.onload = () => {
     input.value = "";
